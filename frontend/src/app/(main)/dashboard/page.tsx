2b2f20ffcfab54897aa7d39d8426e161aa91589f
Turbobot <turbobot@vercel.com>
2025-05-24 09:27:21 +0530

    feat(create-turbo): apply official-starter transform

--- frontend/src/app/(main)/dashboard/page.tsx
+++
@@ -1,8 +0,0 @@
-import { DashboardPage } from '@/components/Dashboard/DashboardPage'
-import React from 'react'
-
-export default function Page() {
-  return (
-    <DashboardPage/>
-  )
-}
--- frontend/src/components/Logo.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export function Logo() {
-  return (
-    <div className="border h-8 w-8 flex items-center justify-center rounded-full transition-colors font-semibold bg-blue-600">
-      <div className="h-5 w-5 rounded-full transition-colors font-semibold bg-primary" />
-    </div>
-  );
-}
